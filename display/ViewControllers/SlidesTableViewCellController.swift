8b36024fe165da52fc472024b130371574cc3067
Ben Hanrahan <banrahan@ist.psu.edu>
2019-02-15 08:21:44 -0500

    add in custom slide cell for the slides table view controller

---
+++ display/ViewControllers/SlidesTableViewCellController.swift
@@ -0,0 +1,16 @@
+//
+//  SlidesTableViewCellController.swift
+//  display
+//
+//  Created by Ben Hanrahan on Friday 2/15/19.
+//  Copyright © 2019 Ning Ma. All rights reserved.
+//
+
+import UIKit
+
+class SlidesTableViewCellController: UITableViewCell {
+    
+    @IBOutlet weak var slideImage: UIImageView!
+    @IBOutlet weak var slideTitle: UILabel!
+    @IBOutlet weak var slideDescription: UILabel!
+}
